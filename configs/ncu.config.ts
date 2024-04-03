b3219fa1b53e585d62efddf916fd213b671ffc9e
electrovir <electrovir@users.noreply.github.com>
2024-04-03 22:54:22 +0000

    [patch] update deps

--- configs/ncu.config.ts
+++ configs/ncu.config.ts
@@ -6,6 +6,7 @@ export const ncuConfig: RunOptions = {
     // exclude these
     reject: [
         ...baseNcuConfig.reject,
+        'typescript',
     ],
     // include only these
     filter: [],
