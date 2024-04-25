4600c752b89eb4c6601a81a428ac63ca1dddbc2e
electrovir <electrovir@users.noreply.github.com>
2024-04-25 20:53:01 +0000

    [dev] dramatically improve vira-book dev load times

--- packages/vira-book/configs/vite.config.ts
+++ packages/vira-book/configs/vite.config.ts
@@ -1,4 +1,4 @@
-import {join} from 'path';
+import {join, resolve} from 'path';
 import {defineConfig} from 'virmator/dist/compiled-base-configs/base-vite';
 
 export default defineConfig({forGitHubPages: false}, (baseConfig, basePaths) => {
@@ -7,6 +7,13 @@ export default defineConfig({forGitHubPages: false}, (baseConfig, basePaths) => 
         base: '/element-vir/vira',
         build: {
             outDir: join(basePaths.cwd, 'dist-book'),
+        },
+        resolve: {
+            alias: {
+                'element-vir': resolve('../element-vir/src/index.ts'),
+                'element-book': resolve('../element-book/src/index.ts'),
+                vira: resolve('../vira/src/index.ts'),
+            },
         },
     };
 });
