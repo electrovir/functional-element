e3ead71df2cde686bf50dd69726317e1ec026283
electrovir <electrovir@users.noreply.github.com>
2024-04-25 20:56:16 +0000

    [minor] add Check24Icon

---
+++ packages/vira/src/icons/icon-svgs/check-24.icon.ts
@@ -0,0 +1,17 @@
+import {html} from 'element-vir';
+import {viraIconCssVars} from '../icon-css-vars';
+import {defineIcon} from '../icon-svg';
+
+export const Check24Icon = defineIcon({
+    name: 'Check24Icon',
+    svgTemplate: html`
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+                d="m17 8.5-7 8-3-3"
+                fill="none"
+                stroke=${viraIconCssVars['vira-icon-stroke-color'].value}
+                stroke-width=${viraIconCssVars['vira-icon-stroke-width'].value}
+            />
+        </svg>
+    `,
+});
--- packages/vira/src/icons/index.ts
+++ packages/vira/src/icons/index.ts
@@ -1,5 +1,6 @@
 /** This file is automatically updated by update-icon-exports.ts */
 
+import {Check24Icon} from './icon-svgs/check-24.icon';
 import {CloseX24Icon} from './icon-svgs/close-x-24.icon';
 import {Element16Icon} from './icon-svgs/element-16.icon';
 import {Element24Icon} from './icon-svgs/element-24.icon';
@@ -13,6 +14,7 @@ import {StatusInProgress24Icon} from './icon-svgs/status-in-progress-24.icon';
 import {StatusSuccess24Icon} from './icon-svgs/status-success-24.icon';
 export * from './icon-css-vars';
 export * from './icon-svg';
+export * from './icon-svgs/check-24.icon';
 export * from './icon-svgs/close-x-24.icon';
 export * from './icon-svgs/element-16.icon';
 export * from './icon-svgs/element-24.icon';
@@ -26,6 +28,7 @@ export * from './icon-svgs/status-in-progress-24.icon';
 export * from './icon-svgs/status-success-24.icon';
 
 export const allIconsByName = {
+    Check24Icon,
     CloseX24Icon,
     Element16Icon,
     Element24Icon,
