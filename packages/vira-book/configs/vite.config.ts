2090894bad629a32873c91e1159387385c35a46d
electrovir <electrovir@users.noreply.github.com>
2024-09-30 12:53:22 +0000

    [dev] update vira-book configs

--- packages/vira-book/configs/vite.config.ts
+++ packages/vira-book/configs/vite.config.ts
@@ -1,19 +1,25 @@
-import {join, resolve} from 'path';
-import {defineConfig} from 'virmator/dist/compiled-base-configs/base-vite';
+import {defineConfig} from '@virmator/frontend/configs/vite.config.base.ts';
+import {join, resolve} from 'node:path';
 
-export default defineConfig({forGitHubPages: false}, (baseConfig, basePaths) => {
-    return {
-        ...baseConfig,
-        base: '/element-vir/vira',
-        build: {
-            outDir: join(basePaths.cwd, 'dist-book'),
-        },
-        resolve: {
-            alias: {
-                'element-vir': resolve('../element-vir/src/index.ts'),
-                'element-book': resolve('../element-book/src/index.ts'),
-                vira: resolve('../vira/src/index.ts'),
+export default defineConfig(
+    {
+        forGitHubPages: false,
+        packageDirPath: resolve(import.meta.dirname, '..'),
+    },
+    (baseConfig, basePaths) => {
+        return {
+            ...baseConfig,
+            base: '/element-vir/vira',
+            build: {
+                outDir: join(basePaths.cwd, 'dist-book'),
             },
-        },
-    };
-});
+            resolve: {
+                alias: {
+                    'element-vir': resolve('../element-vir/src/index.ts'),
+                    'element-book': resolve('../element-book/src/index.ts'),
+                    vira: resolve('../vira/src/index.ts'),
+                },
+            },
+        };
+    },
+);
